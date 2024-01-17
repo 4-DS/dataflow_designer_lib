d1f638b0316cfaea5e2dd8a0d76158280f62072e
buslovaev_ma <buslovaev_ma@outlook.com>
2024-01-17 08:32:17 +0000

    added env for github

--- env.py
+++ env.py
@@ -1,1 +1,2 @@
-SNR_STEP_TEMPLATE='https://github.com/4-DS/pipeline-step_template.git'
+SNR_STEP_TEMPLATE = 'https://github.com/4-DS/pipeline-step_template.git'
+SNR_STEP_TEMPLATE_SUBSTEP = 'do_step.ipynb'
